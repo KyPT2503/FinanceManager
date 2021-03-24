0dadbd857232ebb13fa113edf505c544e189e06b
NKDuy889 <duyd12ql04@gmail.com>
2021-03-24 16:11:21 +0700

    thêm package event ở service và lớp extends và impl

---
+++ src/main/java/com/example/demo/service/event/EventService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.event;
+
+import com.example.demo.model.Event;
+
+import java.util.List;
+
+public class EventService implements IEventService {
+    @Override
+    public List<Event> findAll() {
+        return null;
+    }
+
+    @Override
+    public Event add(Event event) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Event event) {
+        return false;
+    }
+
+    @Override
+    public Event update(Event event) {
+        return null;
+    }
+
+    @Override
+    public Event findById(Long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/demo/service/event/IEventService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.event;
+
+import com.example.demo.model.Event;
+import com.example.demo.service.IGeneralService;
+
+public interface IEventService extends IGeneralService<Event> {
+}
