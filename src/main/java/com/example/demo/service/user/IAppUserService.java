ea45d2ddf36c9605c60840ca6e1f6baf0498265b
vantu <nguyenvantu2972002@gmail.com>
2021-03-29 08:29:48 +0700

    update

--- src/main/java/com/example/demo/service/user/IAppUserService.java
+++ src/main/java/com/example/demo/service/user/IAppUserService.java
@@ -4,8 +4,9 @@ import com.example.demo.model.AppUser;
 import com.example.demo.service.IGeneralService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface  IAppUserService extends IGeneralService<AppUser>,
+public interface IAppUserService extends IGeneralService<AppUser>,
         UserDetailsService {
     AppUser getCurrentUser();
+
     AppUser getUserByUserName(String username);
 }
