3aaee8bf1312ad84067b3e541240528ed6046026
Your Name <you@example.com>
2021-03-29 09:04:28 +0700

    merge previous version with thanh

--- src/main/java/com/example/demo/service/event/IEventService.java
+++ src/main/java/com/example/demo/service/event/IEventService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Event;
 import com.example.demo.dto.EventDTO;
 import com.example.demo.service.IGeneralService;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface IEventService extends IGeneralService<Event> {
@@ -11,4 +12,6 @@ public interface IEventService extends IGeneralService<Event> {
     Event save(Event event,String date,String action, String wallet, String money);
     void deleteById(Long id);
     Event findEventByStringId(String id,String name,String note);
+
+    List<Event> findAllByDateBetween(Date start, Date end);
 }
